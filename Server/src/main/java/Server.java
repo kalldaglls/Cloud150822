5729704c2cb00c11a9696fbb3e343719b63c23a7
Kelvin <kalldaglls@mail.ru>
2022-08-25 15:10:11 +0300

    DZ_3 1.	Клиент и сервер обязаны использовать один и тот же фреймворк? Клиент тоже делать в виде Netty? Код сервера и клиента схож как и в IO? 2.	Откуда направлять(направляется) посылку(а)? Из класса Client самого? Или надо реализовать отдельный класс отправки и сделать его экземпляр в классе Client? 3.	Как происходит «эхо» от сервера? 4.	Как «не сделать» сериализацию? Хочу отправлять файл из Client. 5.	В Netty нужно разбивать посылку на имя-длина имени-… и т.д? 6.	Как из ByteBuf выпилить файл по кусочкам: имя, длина имени, содержимое?

---
+++ Server/src/main/java/Server.java
@@ -0,0 +1,35 @@
+import io.netty.bootstrap.ServerBootstrap;
+
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class Server {
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ServerHandler());
+                        }
+                    })
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            ChannelFuture f = b.bind(7887).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+    //Стандартный кусок кода для всех серверов!!!
+
+    public static void main(String[] args) throws Exception {
+        new Server().run();
+    }
+}
